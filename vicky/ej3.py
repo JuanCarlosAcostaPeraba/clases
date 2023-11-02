1a91e825ea4f616e450340e35e10507b26a73b8f
JuanCarlosAcostaPeraba <juancarlos.ap.dev@gmail.com>
2023-11-02 16:10:47 +0000

    vicky 3

---
+++ vicky/ej3.py
@@ -0,0 +1,20 @@
+matr1 = [
+	[1,1],
+	[1,1]
+]
+matr2 = [
+	[1,1],
+	[1,1]
+]
+
+def sum(matr1, matr2):
+	x = []
+	lista = []
+	contador = 0
+	for i in range(len(matr1)):
+		for j in range(len(matr1[i])):
+			x.append(matr1[i][j] + matr2[i][j])
+		lista.append(x)
+	return lista
+
+print(sum(matr1, matr2))
