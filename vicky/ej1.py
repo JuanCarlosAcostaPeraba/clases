0187a32a01f9744cf8efc3d1f04af79dbc431746
JuanCarlosAcostaPeraba <juancarlos.ap.dev@gmail.com>
2023-11-02 09:55:44 +0000

    initial commit

---
+++ vicky/ej1.py
@@ -0,0 +1,14 @@
+"""
+Crea una función que recibe dos parámetros,
+una cadena de texto y el número de veces que
+se repite la impresión de dicha cadena de texto
+"""
+def funcion(a, b):
+	for i in range(b):
+		print(a)
+		
+
+funcion("hola vicky", 3)
+# "hola vicky"
+# "hola vicky"
+# "hola vicky"
