19ff179a276d3d078900ec9aaf1a56266ab57869
JuanCarlosAcostaPeraba <juancarlos.ap.dev@gmail.com>
2023-11-07 13:45:47 +0000

    Add function to find maximum values between two lists

--- borja/ej.py
+++ borja/ej.py
@@ -1,5 +1,28 @@
-lista = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 1, 2]
+lista1 = [1, 2, 3, 4]
+lista2 = [5, 6, 7, 8, 9]
 
-for i in range(len(lista)):
-	a = sum(lista[:(len(lista)/2)])
-	
+def max(lista1, lista2):
+		lista = []
+
+		if len(lista1) > len(lista2):
+			mayor = lista1
+			menor = lista2
+		else:
+			mayor = lista2
+			menor = lista1
+
+		for i in range(len(mayor)):
+			if i < len(menor):
+				if mayor[i] > menor[i]:
+					lista.append(mayor[i])
+				elif mayor[i] < menor[i]:
+					lista.append(menor[i])
+			else:
+				if mayor[i] > -1:
+					lista.append(mayor[i])
+				elif mayor[i] < -1:
+					lista.append(-1)
+		
+		return lista
+
+max(lista1, lista2)
