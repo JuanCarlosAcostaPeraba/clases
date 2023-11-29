a96192fb5476bbc63346b83a82f6d481a4c34786
JuanCarlosAcostaPeraba <juancarlos.ap.dev@gmail.com>
2023-11-29 11:41:14 +0000

    Refactor variable name in min function

--- borja/ej12.py
+++ borja/ej12.py
@@ -1,11 +1,11 @@
-def min(l):
+def min(lista):
 	'''Devuelve el menor de los elementos de la lista l con recursividad'''
-	if len(l) == 1:
-		return l[0]
+	if len(lista) == 1:
+		return lista[0]
 	else:
-		if l[0] < min(l[1:]):
-			return l[0]
+		if lista[0] < min(lista[1:]):
+			return lista[0]
 		else:
-			return min(l[1:])
+			return min(lista[1:])
 
 print(min([1, -4, 5, 2, 6, 10]))
